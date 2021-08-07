fc672caaf626303ed6f253cb8f9b768ce15d5241
George <linayancen5714@gmail.com>
2021-08-07 18:57:44 +0300

    add first code

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="Head">
+            <img src='https://www.clipartmax.com/png/full/119-1197277_puzzle-icon-teaser-icon-puzzle-character-angel-tube-station.png'></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Profile() {
+    return (
+      <div className="content">
+      <div>
+        <img src="https://i.ytimg.com/vi/uKntdthpcCY/maxresdefault.jpg"></img>
+      </div>
+      <div>
+        ava
+      </div>
+      <div>
+        My post
+      </div>
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Profile;
