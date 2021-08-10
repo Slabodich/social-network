afabe46022c09f0690cf54575e6a5a5eb703e18c
George <linayancen5714@gmail.com>
2021-08-10 15:53:37 +0300

    lesson18

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React, { Profiler } from 'react';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
-      <Profile />
+      <div className='app-content'>
+          <Dialogs />
+      </div>
+      {/*<Profile />*/}
+
     </div >
   );
 }
